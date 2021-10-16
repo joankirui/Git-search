eca473b38f6bde6ac43128f7b7189164de697793
joankirui <joankirui99@gmail.com>
2021-10-16 21:40:05 +0300

    add http requests using promises

--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Repo } from './repo';
 import { User } from './user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { resolve } from 'path';
+import { rejects } from 'assert';
+import { error } from 'console';
 
 
 @Injectable({
@@ -10,5 +15,47 @@ export class HttpServiceService {
   myUser:User [] =[];
   myRepos:Repo [] = [];
 
-  constructor() { }
+  url = 'https://api.github.com/users/';
+  token = '?access_token='+environment.apiUrl;
+
+  constructor(private http:HttpClient) { }
+  joan(userName:string){
+    interface data {
+      name:string;
+      followers_url:string;
+      following_url:string;
+      bio:string;
+    }
+    let promise = new Promise ((resolve, reject)=>{
+      this.myUser = [];
+      this.http.get<data>(this.url+userName+this.token).toPromise().then(
+        (result)=>{
+          this.myUser.push(result);
+          resolve()
+        },
+        (error)=>{
+          reject();
+        })
+    })
+    return promise;
+  }
+  joanRepo(userName:String){
+    interface repoData {
+      name: string;
+      description: string;
+      language: string;
+    }
+    let promise = new Promise ((resolve,reject)=>{
+      this.myRepos = [];
+      this.http.get<repoData>(this.url+userName+"/repos?"+this.token).toPromise().then(
+        (result)=>{
+          this.myRepos.push(result);
+          resolve()
+        },
+        (error)=>{
+          reject();
+        })
+    })
+    return promise;
+  }
 }
