b697733bea51bf1ae9fee9ff92a46a4211d35435
joankirui <joankirui99@gmail.com>
2021-10-16 17:57:50 +0300

    hide the access token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false.valueOf,
+  apiUrl: "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"
 };
 
 /*
