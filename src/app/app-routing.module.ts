05ab3de60f9c5c08d6aaeab0a95215adf8d6c089
joankirui <joankirui99@gmail.com>
2021-10-17 17:47:41 +0300

    update gitignore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReposearchComponent } from './reposearch/reposearch.component';
+import { GitsearchComponent } from './gitsearch/gitsearch.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"user-profile", component:GitsearchComponent},
+{path:"user-profile", component:ReposearchComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
